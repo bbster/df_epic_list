33710d57ac9a110caa1e8da49e26f97592232029
bbster <bbster12@naver.com>
2020-12-14 17:24:46 +0900

    특정 캐릭터 에픽 획득 목록 출력

--- df_epic_list/views.py
+++ df_epic_list/views.py
@@ -1,16 +1,23 @@
+import json
 import urllib
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from pandas import DataFrame as df
 
 class DF_open_api_test(APIView):
     def get(self, reqeust):
-        servicekey = 'IY60cbg7tdVOOcWifeQBnp5PPHXFjbl1'
-        url = 'https://api.neople.co.kr/df/servers/all/characters/wide_swords?apikey=IY60cbg7tdVOOcWifeQBnp5PPHXFjbl1'
+        apikey = 'IY60cbg7tdVOOcWifeQBnp5PPHXFjbl1'
+        url = 'https://api.neople.co.kr/df/servers/cain/characters/853db5c4724d616222cfdd923219509e/timeline?' \
+              'limit=<limit>&code=505&apikey='+apikey
 
         request = urllib.request.Request(url)
         response = urllib.request.urlopen(request)
-        print(response)
-        rescode = response.getcode()
-        return Response(response)
+        print(type(response))
+        raw_data = response.read()
+        encoding = response.info().get_content_charset('utf8')
+        data = json.loads(raw_data.decode(encoding))
+        print(type(data))
+        print(data.len)
+
+        return Response(data)
--- readme.md
+++ readme.md
@@ -3,4 +3,5 @@
 python 3.8.1
 django 3.1.4
 neople_openAPI : https://developers.neople.co.kr/contents/apiDocs
+pandas 1.1.5
 ```
